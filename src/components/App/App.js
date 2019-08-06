5e5b4ea8faae0b3c9d0872d493f2ecd24609d229
Myron <dubtwee@gmail.com>
2019-08-06 14:23:53 -0500

    updating the App componentn markup to be in the appropriate state at the completion of Phase 1.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,40 +6,13 @@ class App extends Component {
         return (
             <div className="scaffold">
                 <div className="scaffold-hd">
-                    <header className="appBar">
-                        <div className="appBar-identity">
-                            <img src={logo} className="logoIcon" alt="logo" />
-                            <h1 className="primeHd">Todo List</h1>
-                        </div>
-                        <div className="appBar-actions">
-                            <nav className="nav">
-                                <ul className="nav-list">
-                                    <li>
-                                        <a href="/Daily" className="navLink">Daily</a>
-                                    </li>
-                                    <li>
-                                        <a href="/Categories" className="navLink">Categories</a>
-                                    </li>
-                                    <li>
-                                        <a href="/Dashboard" className="navLink">Dashboard</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </header>
+                    HEADER
                 </div>
                 <div className="scaffold-bd">
-                    <div className="splash">
-                        <div className="splash-content">
-                            <h2 className="splash-content-hdg">Keep yourself organized</h2>
-                            <button className="btn btn_big">Get Started</button>
-                        </div>
-                    </div>
+                    PAGE BODY
                 </div>
                 <div className="scaffold-ft">
-                    <footer className="appBase">
-                        &copy; Todo List 2019
-                    </footer>
+                    FOOTER
                 </div>
             </div>
         );
