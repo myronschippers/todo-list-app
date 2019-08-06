bd21a006dcb66a7faeaef22dcade09f2ac684f02
Myron <dubtwee@gmail.com>
2019-08-06 09:08:16 -0500

    adding all code for phase 1.1 of the todo list tutorial for reference code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,21 +5,43 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <div className="scaffold">
+                <div className="scaffold-hd">
+                    <header className="appBar">
+                        <div className="appBar-identity">
+                            <img src={logo} className="logoIcon" alt="logo" />
+                            <h1 className="primeHd">Todo List</h1>
+                        </div>
+                        <div className="appBar-actions">
+                            <div className="nav">
+                                <ul className="nav-list">
+                                    <li>
+                                        <a href="/Daily" className="navLink">Daily</a>
+                                    </li>
+                                    <li>
+                                        <a href="/Categories" className="navLink">Categories</a>
+                                    </li>
+                                    <li>
+                                        <a href="/Dashboard" className="navLink">Dashboard</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </header>
+                </div>
+                <div className="scaffold-bd">
+                    <div className="splash">
+                        <div className="splash-content">
+                            <h2 className="splash-content-hdg">Keep yourself organized</h2>
+                            <button className="btn btn_big">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="scaffold-ft">
+                    <footer className="appBase">
+                        &copy; Todo List 2019
+                    </footer>
+                </div>
             </div>
         );
     }
