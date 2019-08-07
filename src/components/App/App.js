3540ba6b0f69600e820c53230060d1b924abbc2b
Myron <dubtwee@gmail.com>
2019-08-07 10:04:36 -0500

    wrapping all of the application with the Router component which will allow us to display particular components based on hash route in our URL

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,39 +12,41 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="scaffold">
-                <div className="scaffold-hd">
-                    <header className="appBar">
-                        <div className="appBar-identity">
-                            <img src={logo} className="logoIcon" alt="logo" />
-                            <h1 className="primeHd">Todo List</h1>
-                        </div>
-                        <div className="appBar-actions">
-                            <nav className="nav">
-                                <ul className="nav-list">
-                                    <li>
-                                        <a href="/Daily" className="navLink">Daily</a>
-                                    </li>
-                                    <li>
-                                        <a href="/Categories" className="navLink">Categories</a>
-                                    </li>
-                                    <li>
-                                        <a href="/Dashboard" className="navLink">Dashboard</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </header>
+            <Router>
+                <div className="scaffold">
+                    <div className="scaffold-hd">
+                        <header className="appBar">
+                            <div className="appBar-identity">
+                                <img src={logo} className="logoIcon" alt="logo" />
+                                <h1 className="primeHd">Todo List</h1>
+                            </div>
+                            <div className="appBar-actions">
+                                <nav className="nav">
+                                    <ul className="nav-list">
+                                        <li>
+                                            <a href="/Daily" className="navLink">Daily</a>
+                                        </li>
+                                        <li>
+                                            <a href="/Categories" className="navLink">Categories</a>
+                                        </li>
+                                        <li>
+                                            <a href="/Dashboard" className="navLink">Dashboard</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </header>
+                    </div>
+                    <div className="scaffold-bd">
+                        <SplashImg hdgText="Keep yourself organized" btnText="Get Started" />
+                    </div>
+                    <div className="scaffold-ft">
+                        <footer className="appBase">
+                            &copy; Todo List 2019
+                        </footer>
+                    </div>
                 </div>
-                <div className="scaffold-bd">
-                    <SplashImg hdgText="Keep yourself organized" btnText="Get Started" />
-                </div>
-                <div className="scaffold-ft">
-                    <footer className="appBase">
-                        &copy; Todo List 2019
-                    </footer>
-                </div>
-            </div>
+            </Router>
         );
     }
 }
