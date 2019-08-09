7269e04efe3413affca3b004054252102e990541
Myron <dubtwee@gmail.com>
2019-08-09 16:53:03 -0500

    creating Footer component and extracting CSS from App for Footer and Header components

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="appBase">
+                &copy; Todo List 2019
+            </footer>
+        );
+    }
+}
+
+export default Footer;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// COMPONENTS
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
 class App extends Component {
     render() {
         return (
             <div className="scaffold">
                 <div className="scaffold-hd">
-                    
+                    <Header />
                 </div>
                 <div className="scaffold-bd">
                     MAIN BODY
                 </div>
                 <div className="scaffold-ft">
-                    <footer className="appBase">
-                        &copy; Todo List 2019
-                    </footer>
+                    <Footer />
                 </div>
             </div>
         );
