485733d39ab1659b6e91527d6d574c26a0e00c16
Myron <dubtwee@gmail.com>
2019-08-10 17:28:52 -0500

    creating a new TodoList component

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                TODO LIST
+            </div>
+        );
+    }
+}
+
+export default TodoList;
