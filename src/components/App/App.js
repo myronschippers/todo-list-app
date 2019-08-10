70a1be07b249037a47890de24e17e51ac5c31fc7
Myron <dubtwee@gmail.com>
2019-08-10 17:13:40 -0500

    reverting changes to make sure they are on the propper branch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,20 +4,8 @@ import './App.css';
 // COMPONENTS
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import TodoList from '../TodoList/TodoList';
-
-const sampleList = [
-    'Mail Letter',
-    'Wash Dishes',
-    'Cut Grass',
-];
 
 class App extends Component {
-    clickAddToList(event) {
-        sampleList.push('Pick Up Dinner');
-        console.log(sampleList);
-    }
-
     render() {
         return (
             <div className="scaffold">
@@ -25,9 +13,7 @@ class App extends Component {
                     <Header />
                 </div>
                 <div className="scaffold-bd">
-                    {JSON.stringify(sampleList)}
-                    <button onClick={this.clickAddToList}>Add Sample</button>
-                    <TodoList />
+                    MAIN BODY
                 </div>
                 <div className="scaffold-ft">
                     <Footer />
